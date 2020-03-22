9e5c3bfbd40725b0c148aa52f888e46da7d145a4
Renat Khusaionv <sokolovskyinprison@list.ru>
2020-03-22 17:51:06 +0500

    wohe

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-let TOKEN = '2f477ee8853a3dc479fa79d466923fe301e6d34fe8c460f2038eee49249afed77eb54dad7a8c774960ae8';
+let TOKEN = '';
 //  
 let GROUPS = ['https://vk.com/public193222366'];
 let MESSAGES = ['Очень приятно видеть это в своей ленте', 'Как красиво', 'Да, я это всегда хотел увидеть', 'чем я занимаюсь'];
