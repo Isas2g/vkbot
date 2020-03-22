8146bb786701d20a75905e9e312d45b3461635cd
Renat Khusaionv <sokolovskyinprison@list.ru>
2020-03-22 18:09:39 +0500

    her

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-let TOKEN = '2f477ee8853a3dc479fa79d466923fe301e6d34fe8c460f2038eee49249afed77eb54dad7a8c774960ae8';
-//  
-let GROUPS = ['https://vk.com/public193222366'];
+let TOKEN = '2fc754eb7329ba8a5cb0fb6c5aeec6b819175bf689c561d7721f9032fff38d348174c6dc7909233896243';
+//  2f477ee8853a3dc479fa79d466923fe301e6d34fe8c460f2038eee49249afed77eb54dad7a8c774960ae8
+let GROUPS = ['https://vk.com/public193240811'];
 let MESSAGES = ['Очень приятно видеть это в своей ленте', 'Как красиво', 'Да, я это всегда хотел увидеть', 'чем я занимаюсь'];
 let MILLISECONDS = 10000; // обновление новостей в миллисекундах
 
