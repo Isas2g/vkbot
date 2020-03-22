0c9ac050bf709337ec1452c99f0177d825edbc99
Renat Khusaionv <sokolovskyinprison@list.ru>
2020-03-22 18:59:57 +0500

    re

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let TOKEN = '7a5c961abbfed03ab4caccc6b6cab4b4b1aa8d649843b6a6fc32a47dd2171b9f47914c95556d885aeae16';
 let GROUPS = ['https://vk.com/public193240811'];
 let MESSAGES = ['Очень приятно видеть это в своей ленте', 'Как красиво', 'Да, я это всегда хотел увидеть', 'чем я занимаюсь'];
-let MILLISECONDS = 1800000; // обновление новостей в миллисекундах
+let MILLISECONDS = 10000; // обновление новостей в миллисекундах
 
 module.exports = {
     TOKEN,
